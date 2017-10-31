aad0880aed37ad93ab6fc92c2487f902064e44d3
cminnelli <cminnelli@gmail.com>
2017-10-30 22:56:04 -0300

    Update nodeMailer.js

--- node_aux/nodeMailer.js
+++ node_aux/nodeMailer.js
@@ -3,8 +3,8 @@ var nodemailer = require('nodemailer');
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'cminnelli@gmail.com',
-    pass: 'alaska77'
+    user: 'test@gmail.com',
+    pass: 'tetstas'
   },
   tls: {
     rejectUnauthorized: false
